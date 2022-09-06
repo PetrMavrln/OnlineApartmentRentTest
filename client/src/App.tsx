b0df2f647586d2503a64c5f6287ff6e5743be476
Petr Mavrin <petr.mavrin@gmail.com>
2022-09-06 18:49:15 +0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './App.css';
 import AppRouter from './components/AppRouter';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <BrowserRouter>
+    // <BrowserRouter>
+    <HashRouter>
       <NavBar />
       <AppRouter />
-    </BrowserRouter>
+    </HashRouter>
+    // </BrowserRouter>
   );
 }
 
